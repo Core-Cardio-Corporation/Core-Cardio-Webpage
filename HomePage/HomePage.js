25b2e1135796876aec54f2ec8dbb2e3fdd69d531
Maeve Johnson <mjohnson534@lsr7.net>
2025-06-25 15:18:36 -0500

    Slideshow animations were implemented
    As well as style changes and more info was added

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -1,14 +1,31 @@
-var slideIndex = 0;
-carousel();
+let slideIndex = 0;
+showSlides();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); 
+function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    if (slides.length > 0) {
+        slides[slideIndex - 1].style.display = "block";
+    }
+    setTimeout(showSlides, 2000);
+}
+
+let slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+    const slides2 = document.getElementsByClassName("mySlides2");
+    for (let i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides2.length) { slideIndex2 = 1; }
+    if (slides2.length > 0) {
+        slides2[slideIndex2 - 1].style.display = "block";
+    }
+    setTimeout(showSlides2, 2000);
 }
